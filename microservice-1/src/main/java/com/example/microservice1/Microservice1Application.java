790801020b1d02d6f93c3e57cec78d2caf24dd11
teplo <malety34@gmail.com>
2023-01-13 00:49:27 +0300

    добавлены манифесты подготовлены dockerfile-s и docker-compose.yml собирается контейнер из образов, но не работает: отваливается второй микрсо, так как не логируется

--- microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
+++ microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Microservice1Application {
+    // TODO: 11.01.2023 по каждому сервису собраться работающий образ
+    // TODO: 11.01.2023 создать головной docker-compose и запустить все образы в одном контейнере
 
+    // TODO: 13.01.2023 удалось запустить контейнер с образами: создать build-ы каждого сервиса
+    // TODO: 13.01.2023 подготовить dockerfile на каждый микросервис
+    // TODO: 13.01.2023 подготовить docker-compose
+
+    // TODO: 13.01.2023 проблема: какой-то сервис отваливается, скорее всего второй, так как не логировался
     public static void main(String[] args) {
         SpringApplication.run(Microservice1Application.class, args);
     }

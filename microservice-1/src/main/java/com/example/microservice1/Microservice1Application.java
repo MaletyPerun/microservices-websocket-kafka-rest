8652853dc6b52f535846fc25c2ccb9812be932cf
teplo <malety34@gmail.com>
2023-01-22 17:02:03 +0300

    инструкция запуска

--- microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
+++ microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Microservice1Application {
-    // TODO: 11.01.2023 создать головной docker-compose и запустить все образы в одном контейнере
 
     public static void main(String[] args) {
         SpringApplication.run(Microservice1Application.class, args);
